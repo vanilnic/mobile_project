519cfe64a44a9f0cd5c7d3d19280eb7f1f9e4af1
vanilnic <bikadog306@gmail.com>
2024-06-15 01:27:55 +0300

    android studio

---
+++ app/src/main/java/com/example/my_expenses/LogInActivity.kt
@@ -0,0 +1,16 @@
+package com.example.my_expenses
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_log_in)
+
+    }
+}
